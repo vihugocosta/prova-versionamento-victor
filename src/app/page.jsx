a1f18759bae6ed23818f1de0758180fd88468c75
Victor Hugo <aluno@senai.br>
2025-07-02 19:17:44 -0300

    feature: criação da página inicial

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,13 @@
 export default function Home() {
   return (
     <>
+    <div className="flex items-center w-full h-screen  justify-center " >
+    <div className="bg-gray-200 flex flex-col gap-[20px] text-center " >
+      <h1 className="text-[36px] font-bold text-gray-800">Prova Versionamento - RPV Senai Cataguases</h1>
+      <p className="text-[24px] text-slate-500 " >Victor Hugo Costa Cruz Menezes</p>
+      <p className="text-[24px] text-slate-400" >Cataguases, 02 de Julho de 2025</p>
+    </div>
+    </div>
     </>
   )
 }
