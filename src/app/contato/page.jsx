c0c02a0ea78fc4c65c75515940493700c08c5d7e
Victor Hugo <aluno@senai.br>
2025-07-02 19:33:32 -0300

    feature: criação da página de contato

---
+++ src/app/contato/page.jsx
@@ -0,0 +1,9 @@
+export default function Contato() {
+    return (
+        <>
+        <div className="flex items-center bg-gray-200 w-full h-screen justify-center">
+        <h1 className="text-[36px] text-slate-800 " >Seja bem-vindo à página de Contato</h1>
+        </div>
+        </>
+    )
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <>
-    <div className="flex items-center w-full h-screen  justify-center " >
-    <div className="bg-gray-200 flex flex-col gap-[20px] text-center " >
+    <div className="flex items-center bg-gray-200 w-full h-screen  justify-center " >
+    <div className=" flex flex-col gap-[20px] text-center " >
       <h1 className="text-[36px] font-bold text-gray-800">Prova Versionamento - RPV Senai Cataguases</h1>
       <p className="text-[24px] text-slate-500 " >Victor Hugo Costa Cruz Menezes</p>
       <p className="text-[24px] text-slate-400" >Cataguases, 02 de Julho de 2025</p>
